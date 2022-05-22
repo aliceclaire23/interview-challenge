db56eba6f3fabba60f5ef857344c57db0ade956a
aliceclaire23 <aliceclaireh@hotmail.co.uk>
2022-05-22 17:27:55 +0100

    create menu builder component

---
+++ src/client/Components/MenuBuilder/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ItemList from '../ItemList';
+
+export default () => (
+  <div className="container menu-builder">
+    <div className="row">
+      <div className="col-4">
+        <div className="filters">
+          <input className="form-control" placeholder="Name" />
+        </div>
+        <ItemList className="item-picker" />
+      </div>
+      <div className="col-8">
+        <h2>Menu preview</h2>
+        <ItemList className="menu-preview" withButton />
+      </div>
+    </div>
+  </div>
+);
