13932f31dfa452668ff92c121fa22f800335d519
aliceclaire23 <aliceclaireh@hotmail.co.uk>
2022-05-22 23:51:56 +0100

    add filter to api endpoint

--- src/server/index.js
+++ src/server/index.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static('dist'));
 
-app.get('/api/items', (req, res) => res.send({ items }));
+app.get('/api/items', (req, res) => {
+  const { filter } = req.query;
+  const filteredItems = items.filter(item => item.name.match(new RegExp(filter, 'i')));
+  res.send({ items: filteredItems })
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
