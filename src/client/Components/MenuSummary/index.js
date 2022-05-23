c5228ebe38a989c44ba1a0c0b50eaebe3fe67ac6
aliceclaire23 <aliceclaireh@hotmail.co.uk>
2022-05-23 01:09:59 +0100

    show dietary type counts in header

--- src/client/Components/MenuSummary/index.js
+++ src/client/Components/MenuSummary/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import DietaryTypeLabel from '../DietaryTypeLabel';
 
-export default ({selectedItems}) => (
-  <div className="menu-summary" data-testid="menu-summary">
-    <div className="container">
-      <div className="row">
-        <div className="col-6 menu-summary-left">
-          <span>{selectedItems.length} items</span>
-        </div>
-        <div className="col-6 menu-summary-right">
-          6x <DietaryTypeLabel dietary='ve' />
-          4x <DietaryTypeLabel dietary='v' />
-          12x <DietaryTypeLabel dietary='n!' />
+const MenuSummary = ({selectedItems}) => {
+  // TODO: get dynamic list of types from the data
+  const dietaryTypes = [ "v", "ve", "df", "gf", "n!", "rsf" ];
+  const dietaries = dietaryTypes.map(type => ({
+    name: type,
+    count: selectedItems.filter(item => item.dietaries.includes(type)).length
+  }));
+  
+  return(
+    <div className="menu-summary" data-testid="menu-summary">
+      <div className="container">
+        <div className="row">
+          <div className="col-6 menu-summary-left">
+            <span>{selectedItems.length} items</span>
+          </div>
+          <div className="col-6 menu-summary-right">
+            { dietaries.map(dietary => (
+              <span key={dietary.name}>
+                {dietary.count}x <DietaryTypeLabel dietary={dietary.name}/>
+              </span>
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  )
+};
+
+export default MenuSummary;
